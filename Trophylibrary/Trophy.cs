30a0e16e26b7f8bcf16be51256a24965c6a406f4
Dasekan Allan Karim <dak003@edu.zealand.dk>
2025-03-05 17:10:17 +0100

    Add project files.

---
+++ Trophylibrary/Trophy.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+namespace Trophylibrary
+{
+    public class Trophy
+    {
+        private int id;
+        private string competition;
+        private int year;
+
+        public int Id { get; set; }
+
+        public string? Competition
+        {
+            get => competition;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Name is null");
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Competition name must be at least 3 characters");
+                }
+                competition = value;
+            }
+        }
+
+        public int Year
+        {
+            get => year;
+            set
+            {
+                if (value < 1970 || value > 2025)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Year), "Year must be between 1970 and 2025");
+                }
+                year = value;
+            }
+        }
+
+        public Trophy(int id, string competition, int year)
+        {
+            this.id = id;
+            this.competition = competition;
+            this.year = year;
+        }
+
+        public Trophy() : this(0, "blank", 0) { }
+
+        public override string ToString()
+        {
+            return $"Trophy: Id = {Id} Competition = {competition}, Year = {year}";
+        }
+
+    }
+}
---
+++ TrophylibraryTests/TrophyTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trophylibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trophylibrary.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestInitialize]
+
+        [TestMethod()]
+        public void CheckCompetitionForNullOrLessThan3Characters()
+        {
+            var trophy = new Trophy();
+
+            // Test null input
+            Assert.ThrowsException<ArgumentNullException>(() => trophy.Competition = null);
+
+            // Test empty string
+            Assert.ThrowsException<ArgumentException>(() => trophy.Competition = "");
+
+            // Test string with less than 3 characters
+            Assert.ThrowsException<ArgumentException>(() => trophy.Competition = "AB");
+        }
+
+        [TestMethod()]
+        public void CheckYearForOutOfRange()
+        {
+            var trophy = new Trophy();
+
+            // Test year less than 1970
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Year = 1969);
+
+            // Test year greater than 2025
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Year = 2026);
+        }
+
+        [TestMethod()]
+        public void ValidYearValuesAreAccepted()
+        {
+            var trophy = new Trophy();
+
+            // Test boundary and mid-range years
+            int[] validYears = { 1970, 2000, 2025 };
+            foreach (var year in validYears)
+            {
+                trophy.Year = year;
+                Assert.AreEqual(year, trophy.Year);
+            }
+        }
+
+  
+
+        [TestMethod()]
+        public void ValidCompetitionValuesAreAccepted()
+        {
+            var trophy = new Trophy();
+
+            // Test valid competition names
+            string[] validCompetitions = { "ABC", "World Championship", "Olympic Games" };
+            foreach (var competition in validCompetitions)
+            {
+                trophy.Competition = competition;
+                Assert.AreEqual(competition, trophy.Competition);
+            }
+        }
+
+        [TestMethod()]
+        public void ToString_Test()
+        {
+            var trophy = new Trophy
+            {
+                Id = 2,
+                Competition = "World Championship",
+                Year = 2024
+            };
+
+            Assert.AreEqual("Trophy: Id = 2 Competition = World Championship, Year = 2024", trophy.ToString());
+        }
+    }
+}
